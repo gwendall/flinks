8fa277267dcea8076bb36502662693f36960182c
gwendall <gwendall.esnault@gmail.com>
2024-06-27 00:23:15 +0200

    downgrade next again

--- webapp/src/app/frames/page.tsx
+++ webapp/src/app/frames/page.tsx
@@ -8,7 +8,7 @@ export default function FrameServerPage() {
 }
 
 const apiUrl = process.env.NEXT_PUBLIC_API_URL as string;
-console.log("apiUrl", apiUrl);
+console.log("apiUrl: ", apiUrl);
 
 type Props = {
     params: { id: string }
@@ -19,12 +19,21 @@ type Props = {
 
 export async function generateMetadata({ params, searchParams }: Props): Promise<Metadata> {
     const { url } = searchParams || {};
+    if (!url) {
+        return {
+            title: "Frame Server Page",
+            description: "A page that renders a frame on the server",
+        };
+    }
+    const frameDataUrl = apiUrl + "/frames?url=" + encodeURIComponent(url);
     const frameData: {
         status: string;
         frame: Frame;
-    } = await axios.get(apiUrl + "/frames?url=" + encodeURIComponent(url)).then(res => res.data);
+    } = await axios.get(frameDataUrl).then(res => res.data).catch(err => {
+        console.error(err);
+    });
     return {
-        title: frameData.frame?.title,
+        title: frameData?.frame?.title || "Frame Server Page",
         description: "A page that renders a frame on the server",
     }
 }
