da1ad1b0f5e572e9469c6e86e4accc2ee8626bd2
gwendall <gwendall.esnault@gmail.com>
2024-06-27 07:27:23 +0200

    Update client.tsx

--- webapp/src/app/client.tsx
+++ webapp/src/app/client.tsx
@@ -45,7 +45,7 @@ const PageContent = styled.div`
     background-color: rgba(255, 255, 255, 0.5);
     border: 1px solid rgba(255, 255, 255, 0.7);
     border-radius: 30px;
-    padding: 30px;
+    padding: 20px 30px;
     backdrop-filter: blur(10px);
 `;
 
@@ -122,7 +122,9 @@ export default function HomePageClient() {
                         marginTop: 10,
                         color: 'rgba(0, 0, 0, 0.5)',
                         fontSize: '0.9rem',
-                    }}>or view the codebase</div>
+                    }}>
+                        or view the codebase
+                    </div>
                 </PageContent>
             </PageContainer>
         </BackgroundVideo>
