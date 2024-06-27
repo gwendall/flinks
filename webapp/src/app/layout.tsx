30c55c9f414f236b41ec6319a55741261adb03b5
gwendall <gwendall.esnault@gmail.com>
2024-06-27 18:02:46 +0200

    fix metadata

--- webapp/src/app/layout.tsx
+++ webapp/src/app/layout.tsx
@@ -1,19 +1,15 @@
-"use client";
-
-import { Inter, Roboto } from "next/font/google";
+import { Inter } from "next/font/google";
 import StyledComponentsRegistry from "@/lib/registry";
 import GlobalStyle from "@/components/GlobalStyle";
 import ReactQueryProvider from "@/components/ReactQueryProvider";
 import React from "react";
+import type { Metadata } from "next";
+import { appUrl } from "@/utils/const";
 import NeynarContainer from "@/components/NeynarContainer";
 import "./globals.css";
 import "@neynar/react/dist/style.css";
 
 const inter = Inter({ subsets: ["latin"] });
-const roboto = Roboto({
-  subsets: ["latin"],
-  weight: ["100", "300", "500", "700"]
-});
 
 export default function RootLayout({
   children,
@@ -37,3 +33,23 @@ export default function RootLayout({
     </html>
   );
 }
+
+export const metadata: Metadata = {
+  title: "Flinks | Frames across the web",
+  description: "Interact with Farcaster Frames, all across the web.",
+  metadataBase: new URL(appUrl),
+  icons: {
+    icon: '/favicon.ico',
+  },
+  openGraph: {
+    images: '/logo.png',
+  },
+  twitter: {
+    title: "Flinks | Frames across the web",
+    description: "Interact with Farcaster Frames, all across the web.",
+    images: ['/logo.png'],
+    creator: '@gwendall',
+    card: 'summary_large_image',
+  }
+
+};
--- webapp/src/app/page.tsx
+++ webapp/src/app/page.tsx
@@ -1,28 +1,6 @@
 import React from "react"
-import type { Metadata } from "next";
 import HomePageClient from "./client";
-import { appUrl } from "@/utils/const";
 
 export default function HomePageServer() {
   return <HomePageClient />
 }
-
-export const metadata: Metadata = {
-  title: "Flinks | Frames across the web",
-  description: "Interact with Farcaster Frames, all across the web.",
-  metadataBase: new URL(appUrl),
-  icons: {
-    icon: '/favicon.ico',
-  },
-  openGraph: {
-    images: '/logo.png',
-  },
-  twitter: {
-    title: "Flinks | Frames across the web",
-    description: "Interact with Farcaster Frames, all across the web.",
-    images: ['/logo.png'],
-    creator: '@gwendall',
-    card: 'summary_large_image',
-  }
-
-};
