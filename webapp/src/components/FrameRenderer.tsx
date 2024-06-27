8def0a8e81cad64c8700b0749ea928e67a37b8f5
gwendall <gwendall.esnault@gmail.com>
2024-06-27 05:36:26 +0200

    loading next frame message

--- webapp/src/components/FrameRenderer.tsx
+++ webapp/src/components/FrameRenderer.tsx
@@ -4,6 +4,7 @@ import styled from "styled-components";
 import type { ImgHTMLAttributes } from "react";
 import type { Frame, FrameButton } from "frames.js";
 import { FrameStackMessage, FrameStackRequestError, FrameState } from "@frames.js/render";
+import { AnimatePresence, motion } from "framer-motion";
 
 function getErrorMessageFromFramesStackItem(
     item: FrameStackMessage | FrameStackRequestError
@@ -21,6 +22,8 @@ function getErrorMessageFromFramesStackItem(
 
 const ImageContainer = styled.div`
     flex: 1;
+    flex-grow: 1;
+    flex-shrink: 0;
     position: relative;
     img {
         position: absolute;
@@ -72,7 +75,7 @@ const Button = styled.button`
     }
 `;
 
-export const StatusText = styled.div`
+export const StatusText = styled(motion.div)`
     width: 100%;
     height: 100%;
     padding: 15px;
@@ -108,7 +111,7 @@ const FrameInput = styled.input`
     }
 `;
 
-const ActionsContainer = styled.div`
+const ActionsContainer = styled(motion.div)`
     background-color: rgb(42, 36, 50);
     border-top: 1px solid rgba(255, 255, 255, 0.1);
 `;
@@ -182,117 +185,124 @@ export function FrameRenderer({
                 ? currentFrame.request.sourceFrame
                 : undefined;
     }
+    const renderedImage = debugImage ?? frame?.image;
     return (
-        <>
-            <ImageContainer>
+        <AnimatePresence mode="wait">
+            <ImageContainer
+            // key={`image-${frameState.currentFrameStackItem?.url}-${renderedImage}`}
+            >
                 {currentFrame.status === "message" ? (
                     <StatusText>{getErrorMessageFromFramesStackItem(currentFrame)}</StatusText>
                 ) : null}
-                {!!frame && !!frame.image && (
-                    <img
-                        src={debugImage ?? frame.image}
-                        key={debugImage ?? frame.image}
-                        alt="Frame image"
-                        style={{
-                            filter: isImageLoading ? "blur(4px)" : undefined,
-                            opacity: isImageError ? 0 : 1
-                        }}
-                        onLoadStart={() => {
-                            setIsImageLoading(true);
-                        }}
-                        onLoad={() => {
-                            setIsImageLoading(false);
-                        }}
-                        onError={() => {
-                            setIsImageLoading(false);
-                            setIsImageError(true);
-                        }}
-                    />
-                )}
+                <img
+                    src={renderedImage}
+                    key={`actions-${frameState.currentFrameStackItem?.url}-${renderedImage}`}
+                    alt="Frame image"
+                    style={{
+                        filter: isImageLoading ? "blur(4px)" : undefined,
+                        opacity: !renderedImage || isImageError ? 0 : 1
+                    }}
+                    onLoadStart={() => {
+                        setIsImageLoading(true);
+                    }}
+                    onLoad={() => {
+                        setIsImageLoading(false);
+                    }}
+                    onError={() => {
+                        setIsImageLoading(false);
+                        setIsImageError(true);
+                    }}
+                />
                 {!!frame && isImageError ? (
                     <StatusText>{frame.title}</StatusText>
                 ) : null}
                 {!!frame && isImageLoading ? (
                     <StatusText>Loading image...</StatusText>
                 ) : null}
+                {currentFrame.status === "pending" ? (
+                    <StatusText>Loading next frame</StatusText>
+                ) : null}
             </ImageContainer>
-            {!!frame ? (
-                <ActionsContainer>
-                    {frame.inputText ? (
-                        <FrameInputContainer>
-                            <FrameInput
-                                value={frameState.inputText}
-                                type="text"
-                                placeholder={frame.inputText}
-                                onChange={(e) => {
-                                    frameState.setInputText(e.target.value);
-                                }}
-                            />
-                        </FrameInputContainer>
-                    ) : null}
-                    {!!frame.buttons && frame.buttons.length > 0 ? (
-                        <ButtonsContainer>
-                            <ButtonsInner>
-                                {frame.buttons.map((frameButton: FrameButton, index: number) => (
-                                    <Button
-                                        type="button"
-                                        disabled={isLoading}
-                                        style={{
-                                            cursor: isLoading ? undefined : "pointer",
-                                            opacity: isLoading ? 0.5 : 1,
-                                        }}
-                                        onClick={() => {
-                                            if (isInIframe && frameButton.action === 'link') {
-                                                window.parent.postMessage({
-                                                    type: 'openNewFlinkUrl',
-                                                    url: frameButton.target,
-                                                }, "*");
-                                            } else {
-                                                Promise.resolve(
-                                                    frameState.onButtonPress(
-                                                        // Partial frame could have enough data to handle button press
-                                                        frame as Frame,
-                                                        frameButton,
-                                                        index
-                                                    )
-                                                ).catch((e: unknown) => {
-                                                    // eslint-disable-next-line no-console -- provide feedback to the user
-                                                    console.error('Error clicking button.', e);
-                                                });
-                                            }
-                                        }}
-                                        // eslint-disable-next-line react/no-array-index-key -- this is fine
-                                        key={index}
-                                    >
-                                        {frameButton.action === "mint" ? `⬗ ` : ""}
-                                        {frameButton.label}
-                                        {frameButton.action === "tx" ? (
-                                            <svg
-                                                aria-hidden="true"
-                                                focusable="false"
-                                                role="img"
-                                                viewBox="0 0 16 16"
-                                                className="ml-1 mb-[2px] text-gray-400 inline-block select-none align-text-middle overflow-visible"
-                                                width="12"
-                                                height="12"
-                                                fill="currentColor"
-                                            >
-                                                <path d="M9.504.43a1.516 1.516 0 0 1 2.437 1.713L10.415 5.5h2.123c1.57 0 2.346 1.909 1.22 3.004l-7.34 7.142a1.249 1.249 0 0 1-.871.354h-.302a1.25 1.25 0 0 1-1.157-1.723L5.633 10.5H3.462c-1.57 0-2.346-1.909-1.22-3.004L9.503.429Zm1.047 1.074L3.286 8.571A.25.25 0 0 0 3.462 9H6.75a.75.75 0 0 1 .694 1.034l-1.713 4.188 6.982-6.793A.25.25 0 0 0 12.538 7H9.25a.75.75 0 0 1-.683-1.06l2.008-4.418.003-.006a.036.036 0 0 0-.004-.009l-.006-.006-.008-.001c-.003 0-.006.002-.009.004Z" />
-                                            </svg>
-                                        ) : (
-                                            ""
-                                        )}
-                                        {frameButton.action === "post_redirect" ||
-                                            frameButton.action === "link"
-                                            ? ` ↗`
-                                            : ""}
-                                    </Button>
-                                ))}
-                            </ButtonsInner>
-                        </ButtonsContainer>
-                    ) : null}
-                </ActionsContainer>
-            ) : null}
-        </>
+            <ActionsContainer
+                // key={`actions-${frameState.currentFrameStackItem?.url}`}
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+            >
+                {frame?.inputText ? (
+                    <FrameInputContainer>
+                        <FrameInput
+                            value={frameState.inputText}
+                            type="text"
+                            placeholder={frame.inputText}
+                            onChange={(e) => {
+                                frameState.setInputText(e.target.value);
+                            }}
+                        />
+                    </FrameInputContainer>
+                ) : null}
+                {!!frame?.buttons && frame.buttons.length > 0 ? (
+                    <ButtonsContainer>
+                        <ButtonsInner>
+                            {frame.buttons.map((frameButton: FrameButton, index: number) => (
+                                <Button
+                                    type="button"
+                                    disabled={isLoading}
+                                    style={{
+                                        cursor: isLoading ? undefined : "pointer",
+                                        opacity: isLoading ? 0.5 : 1,
+                                    }}
+                                    onClick={() => {
+                                        if (isInIframe && frameButton.action === 'link') {
+                                            window.parent.postMessage({
+                                                type: 'openNewFlinkUrl',
+                                                url: frameButton.target,
+                                            }, "*");
+                                        } else {
+                                            Promise.resolve(
+                                                frameState.onButtonPress(
+                                                    // Partial frame could have enough data to handle button press
+                                                    frame as Frame,
+                                                    frameButton,
+                                                    index
+                                                )
+                                            ).catch((e: unknown) => {
+                                                // eslint-disable-next-line no-console -- provide feedback to the user
+                                                console.error('Error clicking button.', e);
+                                            });
+                                        }
+                                    }}
+                                    // eslint-disable-next-line react/no-array-index-key -- this is fine
+                                    key={index}
+                                >
+                                    {frameButton.action === "mint" ? `⬗ ` : ""}
+                                    {frameButton.label}
+                                    {frameButton.action === "tx" ? (
+                                        <svg
+                                            aria-hidden="true"
+                                            focusable="false"
+                                            role="img"
+                                            viewBox="0 0 16 16"
+                                            className="ml-1 mb-[2px] text-gray-400 inline-block select-none align-text-middle overflow-visible"
+                                            width="12"
+                                            height="12"
+                                            fill="currentColor"
+                                        >
+                                            <path d="M9.504.43a1.516 1.516 0 0 1 2.437 1.713L10.415 5.5h2.123c1.57 0 2.346 1.909 1.22 3.004l-7.34 7.142a1.249 1.249 0 0 1-.871.354h-.302a1.25 1.25 0 0 1-1.157-1.723L5.633 10.5H3.462c-1.57 0-2.346-1.909-1.22-3.004L9.503.429Zm1.047 1.074L3.286 8.571A.25.25 0 0 0 3.462 9H6.75a.75.75 0 0 1 .694 1.034l-1.713 4.188 6.982-6.793A.25.25 0 0 0 12.538 7H9.25a.75.75 0 0 1-.683-1.06l2.008-4.418.003-.006a.036.036 0 0 0-.004-.009l-.006-.006-.008-.001c-.003 0-.006.002-.009.004Z" />
+                                        </svg>
+                                    ) : (
+                                        ""
+                                    )}
+                                    {frameButton.action === "post_redirect" ||
+                                        frameButton.action === "link"
+                                        ? ` ↗`
+                                        : ""}
+                                </Button>
+                            ))}
+                        </ButtonsInner>
+                    </ButtonsContainer>
+                ) : null}
+            </ActionsContainer>
+        </AnimatePresence>
     );
 }
