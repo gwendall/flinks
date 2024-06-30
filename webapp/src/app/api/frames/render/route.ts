7a2123b7ca8c6bc8c4a52658e0c101b1b5c8a99d
gwendall <gwendall.esnault@gmail.com>
2024-06-30 04:05:42 +0200

    Update route.ts

--- webapp/src/app/api/frames/render/route.ts
+++ webapp/src/app/api/frames/render/route.ts
@@ -1,1 +1,13 @@
-export { GET, POST } from "@frames.js/render/next";
+import { GET as frameGET, POST as framePOST } from "@frames.js/render/next";
+import { NextRequest } from "next/server";
+
+export async function GET(request: NextRequest) {
+    console.log("GET request received");
+    return frameGET(request);
+}
+
+export async function POST(request: NextRequest) {
+    const body = await request.json();
+    console.log("POST request received", JSON.stringify(body, null, 2));
+    return framePOST(request);
+}
